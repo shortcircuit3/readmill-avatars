0f11325b2719ad63b8e47a0e158edfe3357a70af
Alex Miles <howdy@alexmilesdesign.com>
2013-09-20 15:16:44 +0200

    Site updated to e71c7b7

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -27,13 +27,16 @@ $(function() {
               var userID = data.items[i].user.id;
               var firstname = data.items[i].user.firstname || '*';
               var lastname = data.items[i].user.lastname || '*';
+              var currentlyReading = data.items[i].user.books_reading_count;
+              var finishedBooks = data.items[i].user.books_finished_count;
+              var totalBooks = currentlyReading + finishedBooks;
 
               var avatarURL = "https://api.readmill.com/v2/users/" + userID + "/avatar?size=large&client_id=47c04f7eb4a2f8710a1a8fdf61244c37";
               $(".avatars").append(
                 '<li>' +
                   '<a href="#_"><img id="'+ userID +'" src="'+ avatarURL +'"/></a>' +
                   '<p class="name">' +  firstname + ' ' + lastname + '</p>' +
-                  '<p>' + userID + '</p>'+
+                  '<p>' + totalBooks + ' books</p>'+
                 '</li>'
               );
             }
