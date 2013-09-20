772f29f6fba2a92d1f0e40307edc230f0b7dfd00
Alex Miles <howdy@alexmilesdesign.com>
2013-09-20 14:40:51 +0200

    Add refresh user button

--- source/javascripts/scripts.js
+++ source/javascripts/scripts.js
@@ -2,6 +2,11 @@ $(function() {
   // The initial request URL
 
   function getAvatars(user) {
+
+    // Empty existing avatars from container
+    $(".avatars").empty();
+
+    // Request URL
     var reqURL = "https://api.readmill.com/v2/users/"+ user +"/followings?client_id=47c04f7eb4a2f8710a1a8fdf61244c37";
 
     // Retreives photos from Instagram
@@ -14,11 +19,19 @@ $(function() {
           url: reqURL,
           success: function(data) {
             console.log(data);
+
             // Get the amount of photos in the object
             var count = data.items.length;
 
             for (var i = 0; i < count; i++) {
               var userID = data.items[i].user.id;
+
+              // If the user is the editor, return false
+              // if (userID === 74485) {
+              //   refreshAvatars();
+              //   return false;
+              // }
+
               var avatarURL = "https://api.readmill.com/v2/users/" + userID + "/avatar?size=large&client_id=47c04f7eb4a2f8710a1a8fdf61244c37";
               $(".avatars").append('<li><a href="#_"><img id="'+ userID +'" src="'+ avatarURL +'"/></a><p>' + userID + '</p></li>');
             }
@@ -28,11 +41,24 @@ $(function() {
     loadAvatars(reqURL);
   }
 
-  getAvatars(5702);
+  function refreshAvatars() {
+    var randomUserID = Math.floor(Math.random() * 10000);
+    console.log(randomUserID);
+    getAvatars(randomUserID);
+  }
 
+  // View 'this' the avatars that this user is following
   $('body').on('click', 'img', function(){
     var newUserID = this.id;
-    $(".avatars").empty();
     getAvatars(newUserID);
   });
+
+  // Refresh view
+  $('body').on('click', '.refresh', function(){
+    refreshAvatars();
+  });
+
+  // Initialize view
+  getAvatars(5702);
+
 });
